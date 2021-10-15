9add439975fb93892d439158cc5bac557f848091
Szymon Bełus <sbelus@gmail.com>
2021-10-15 11:04:56 +0200

    Generate more complicated and random password

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -155,7 +155,16 @@ URL: {1}
 
     private string GeneratePassword()
     {
-      return "pizza";
+      const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+      StringBuilder res = new StringBuilder();
+      Random rnd = new Random();
+      var length = 10;
+      while (0 < length--)
+      {
+        res.Append(valid[rnd.Next(valid.Length)]);
+      }
+      return res.ToString();
+
     }
   }
 }
